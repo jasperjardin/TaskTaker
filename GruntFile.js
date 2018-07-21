ea45d2a80e6712c73f2a3337c2c95ee26b73997a
jasperjardin <jasper.jardin1994@gmail.com>
2018-07-21 15:59:59 +0800

    Updated the GruntFile.js for the CSS and SASS.

--- GruntFile.js
+++ GruntFile.js
@@ -31,9 +31,14 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 };
-/*Dunhakdis Set-up*/
-/*1.0 Use [npm install grunt --save-dev]*/
-/*1.1 Use [npm install grunt-sass --save-dev]*/
-/*1.2 Use [npm install grunt-contrib-watch --save-dev]*/
-/*2.0 Use Grunt command to start a project*/
-/*  npm install grunt --save-dev; npm install grunt-sass --save-dev; npm install grunt-contrib-watch --save-dev; */
+/*
+SASS to CSS Project Set-up
+    1.0: Step by step NPM install
+        1.1: Use [npm install grunt --save-dev]
+        1.2: Use [npm install grunt-sass --save-dev]
+        1.3: Use [npm install grunt-contrib-watch --save-dev]
+    2.0: Shorthand NPM Install
+        2.1: npm install grunt --save-dev; npm install grunt-sass --save-dev; npm install grunt-contrib-watch --save-dev;
+    3.0: Use "Grunt" command to start your project.
+        3.1: Type "Grunt"
+*/
