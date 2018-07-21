7179861fd717a45fb594320924121af03b5c048f
jasperjardin <jasper.jardin1994@gmail.com>
2018-07-21 16:00:51 +0800

    Added CRUD methods to the Tasks class and some extra methods.

--- classes/Objects/Tasks.php
+++ classes/Objects/Tasks.php
@@ -1,9 +1,13 @@
 <?php
 namespace Tasks\Objects;
 
+include_once( TASKS_TAKER_PATH . 'classes/Navigations/Pagination.php');
+
 use \PDO;
 
 use Tasks\CRUD\DataHandler as DataHandler;
+
+use Tasks\Navigations\Pagination as Pagination;
 
 /**
  * Class for Create, Read, Update and Delete.
@@ -19,6 +23,7 @@ class Tasks
 {
     // object properties
     protected $handler;
+    protected $last_insert_id;
     protected $table = 'tasks';
     public $id;
     public $title;
@@ -56,7 +61,8 @@ class Tasks
         $statement->bind( ':status', $this->status );
         $statement->bind( ':position', $this->position);
 
-        if( $statement->execute() ){
+        if ( $statement->execute() ) {
+            $this->setLastInsertId( $statement->lastInsertId() );
             $statement->endTransaction();
             return true;
         }
@@ -64,21 +70,49 @@ class Tasks
         return false;
     }
 
-    public function readAll( $page = '' )
+    public function readAll( $page_num = '', $items_per_page = '', $order_by = 'id', $order = 'ASC' )
     {
-        $result = '';
-        $offset = ( TASKS_PER_PAGE * $page ) - TASKS_PER_PAGE;
-        $query = "SELECT * FROM {$this->table} LIMIT ". $offset .", " . TASKS_PER_PAGE;
+        $statement = '';
+        $offset    = '';
+        $fetch_all = array();
+        $query     = "SELECT * FROM {$this->table}";
 
-        if ( empty( $page ) ) {
-            $query = "SELECT * FROM tasks";
+        if ( ! is_numeric( $items_per_page ) ) {
+            $items_per_page = TASKTAKER_TASKS_PER_PAGE;
         }
 
-        $statement = $this->handler;
-        $statement->query( $query );
-        $result = $statement->resultset();
+        $query .= ' ORDER BY ' . $order_by . ' ' . $order;
 
-        return $result;
+        if ( is_numeric( $page_num ) && 0 !== $page_num ) {
+            $offset = ( $items_per_page * $page_num ) - $items_per_page;
+            $query .= " LIMIT ". $offset .", " . $items_per_page;
+        }
+
+
+        if ( 0 !== $page_num ) {
+            $statement = $this->handler;
+            $statement->query( $query );
+            $fetch_all['tasks'] = $statement->resultset();
+        }
+
+        if ( empty( $fetch_all['tasks'] ) ) {
+            $fetch_all['tasks'] = array(
+                'error' => 'There are no task to display.',
+            );
+        }
+
+        return $fetch_all;
+    }
+
+    public function count()
+    {
+        $statement = $this->handler;
+        $query     = "SELECT * FROM tasks";
+
+        $statement->query( $query );
+        $statement->resultset();
+
+        return $statement->rowCount();
     }
 
     public function read( $id )
@@ -131,7 +165,7 @@ class Tasks
         $statement->bind( ':position', $this->position);
         $statement->bind( ':id', $this->id );
 
-        if( $statement->execute() ){
+        if ( $statement->execute() ) {
             $statement->endTransaction();
             return true;
         }
@@ -151,11 +185,76 @@ class Tasks
         $this->id = filter_var( $this->id, FILTER_VALIDATE_INT );
         $statement->bind( ':id', $this->id );
 
-        if( $statement->execute() ){
+        if ( $statement->execute() ) {
             $statement->endTransaction();
             return true;
         }
 
         return false;
+    }
+
+    private function setLastInsertId( $last_insert_id = '' )
+    {
+        if ( is_numeric( $last_insert_id ) ) {
+            $this->last_insert_id = $last_insert_id;
+        }
+        return;
+    }
+
+    public function getLastInsertId()
+    {
+        return $this->last_insert_id;
+    }
+
+    private function getPaginationArgs( $set_args = array() )
+    {
+        $actual_link = (
+            isset(
+                $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] === 'on'
+                ? "https" : "http"
+            ) . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+        $default_args = array(
+            'items_per_page' => 5,
+            'page_number'    => 1,
+            'total_items'    => 13,
+            'page_range'     => 5,
+            'page_url'       => $actual_link,
+        );
+
+        $args = array_merge( $default_args, $set_args );
+
+        $pagination = new Pagination();
+        $pagination->setPageURL( $args[ 'page_url' ] );
+        $pagination->setItemsPerPage( $args[ 'items_per_page' ] );
+        $pagination->setPageNumber( $args[ 'page_number' ] );
+        $pagination->setTotalItems( $args[ 'total_items' ] );
+        $pagination->setPageRange( $args[ 'page_range' ] );
+
+        return $pagination;
+    }
+
+    public function paginate( $set_args = array() )
+    {
+        $pagination = $this->getPaginationArgs( $set_args );
+
+        return $pagination->paginate();
+    }
+
+    public function paginateArray( $set_args = array() )
+    {
+        $pagination = $this->getPaginationArgs( $set_args );
+
+        return $pagination->paginateArray();
+    }
+
+    public function getLastPagination( $set_args = array() )
+    {
+        $pagination = $this->getPaginationArgs( $set_args );
+        $last_pagination = array(
+            'page' => $pagination->getLastPagination(),
+        );
+
+        return $last_pagination;
     }
 }
