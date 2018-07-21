5a65bb4f00b0ee42c0f1756a5b59d12305a696b3
jasperjardin <jasper.jardin1994@gmail.com>
2018-07-21 16:17:00 +0800

    Holds the all the variables use for the Javascript of the app.

---
+++ js/components/variables.js
@@ -0,0 +1,8 @@
+    /**
+     * Define Variables
+     */
+    var tasktaker_page_num      = 1;
+    var tasktaker_tasks_url     = 'RestAPI/tasks.php';
+    var tasktaker_last_page_url = '';
+    var last_pagination         = '';
+    var last_page               = '';
